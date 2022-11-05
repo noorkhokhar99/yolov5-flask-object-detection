1cc9befeef3d1aac7c3b44dac801063abb4fe00b
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2022-11-05 12:50:32 +0500

    Add files via upload

---
+++ webapp.py
@@ -0,0 +1,43 @@
+"""
+Simple app to upload an image via a web form 
+and view the inference results on the image in the browser.
+"""
+import argparse
+import io
+import os
+from PIL import Image
+
+import torch
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return redirect(request.url)
+        file = request.files["file"]
+        if not file:
+            return
+
+        img_bytes = file.read()
+        img = Image.open(io.BytesIO(img_bytes))
+        results = model([img])
+
+        results.render()  # updates results.imgs with boxes and labels
+        results.save(save_dir="static/")
+        return redirect("static/image0.jpg")
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
+    parser.add_argument("--port", default=5000, type=int, help="port number")
+    args = parser.parse_args()
+
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)  # force_reload = recache latest code
+    model.eval()
+    app.run(host="0.0.0.0", port=args.port)  # debug=True causes Restarting with stat
